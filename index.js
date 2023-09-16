969422ade16e11e3948f85896b415b868a215320
LydianLights <rane.fields@gmail.com>
2023-09-16 18:21:47 -0400

    Reorganize code

--- index.js
+++ index.js
@@ -1,110 +1,158 @@
-const mainVideo = document.querySelector("div[data-test-selector=video-player__video-container] video");
+let mainVideo = null;
 let miniVideo = null;
 let miniVideoContainer = null;
 let adText = null;
 let muteButton = null;
 let volumeSlider = null;
 let customSlider = null;
-let volumeHandler = null;
 
 let blocking = false;
+let restoreVolume = null;
+let href = "";
 
-function main() {
+let debugMessages = true;
+
+// ==== MAIN LOOP ==== //
+setInterval(function () {
+    if (href !== location.href) {
+        newPage();
+    }
+
     if (!mainVideo) {
-        console.error("Simple Twitch Adblocker: Could not find main video player.");
         return;
     }
 
-    setInterval(function () {
-        adText = document.querySelector("span[data-test-selector=ad-banner-default-text]");
+    adText = document.querySelector("span[data-test-selector=ad-banner-default-text]");
 
-        if (adText && !blocking) {
-            miniVideo = document.querySelector("div[data-test-selector=picture-by-picture-player-container] video");
-            miniVideoContainer = document.querySelector("div[data-test-selector=picture-by-picture-player-background]");
-            muteButton = document.querySelector("button[data-a-target=player-mute-unmute-button]");
-            volumeSlider = document.querySelector("input[data-a-target=player-volume-slider]");
+    if (adText && !blocking) {
+        startBlocking();
+    }
 
-            blocking = true;
+    if (!adText && blocking) {
+        stopBlocking();
+    }
+}, 500);
 
-            if (!miniVideo) {
-                adText.textContent = "No mini player found -- auto muting instead. ";
-                mainVideo.muted = true;
-                return;
-            }
-
-            adText.textContent = "Blocking ads. ";
-            mainVideo.insertAdjacentElement("afterend", miniVideo);
-
-            miniVideo.muted = false;
-            miniVideo.volume = mainVideo.volume;
-            mainVideo.muted = true;
-
-            volumeHandler = () => {
-                if (!blocking) return;
-                mainVideo.muted = true;
-            };
-            mainVideo.addEventListener("volumechange", volumeHandler, false);
-
-            if (miniVideoContainer) {
-                miniVideoContainer.style.cssText = "display: none !important;";
-            }
-
-            if (muteButton && volumeSlider) {
-                muteButton.style.cssText = "display: none !important;";
-                volumeSlider.style.cssText = "display: none !important;";
-
-                customSlider = document.createElement("input");
-                customSlider.type = "range";
-                customSlider.min = 0;
-                customSlider.max = 1;
-                customSlider.step = 0.01;
-                customSlider.value = miniVideo.volume;
-                customSlider.className = "STA__custom-slider";
-
-                customSlider.addEventListener("input", function () {
-                    miniVideo.volume = customSlider.value;
-                }, false);
-
-                muteButton.insertAdjacentElement("afterend", customSlider);
-            }
-
-            return;
+// ==== HELPERS ==== //
+function log(msg) {
+    if (debugMessages) {
+        if (typeof msg === "string") {
+            console.log(`[Simple Twitch Adblocker] ${msg}`);
+        } else {
+            console.log(msg);
         }
-
-        if (!adText && blocking) {
-            blocking = false;
-
-            if (volumeHandler) {
-                mainVideo.removeEventListener("volumechange", volumeHandler, false);
-            }
-
-            if (miniVideo) {
-                mainVideo.muted = miniVideo.volume === 0;
-                mainVideo.volume = miniVideo.volume;
-                miniVideo.remove();
-            }
-
-            if (muteButton) {
-                muteButton.style.cssText = "";
-            }
-
-            if (volumeSlider) {
-                volumeSlider.style.cssText = "";
-            }
-
-            if (customSlider) {
-                customSlider.remove();
-            }
-
-            miniVideo = null;
-            miniVideoContainer = null;
-            adText = null;
-            muteButton = null;
-            volumeSlider = null;
-            customSlider = null;
-            volumeHandler = null;
-        }
-    }, 500);
+    }
 }
 
-main();
+function resetRefs() {
+    miniVideo = null;
+    miniVideoContainer = null;
+    adText = null;
+    muteButton = null;
+    volumeSlider = null;
+    customSlider = null;
+
+    restoreVolume = null;
+}
+
+function volumeHandler() {
+    if (blocking) {
+        mainVideo.muted = true;
+    }
+}
+
+// ==== EVENTS ==== //
+function newPage() {
+    href = location.href;
+    log(`Page loaded: ${href}`);
+    blocking = false;
+    resetRefs();
+    mainVideo = document.querySelector("div[data-a-target=video-player][data-a-player-type=site] video");
+    log("Main player:")
+    log(mainVideo);
+    mainVideo.addEventListener("volumechange", volumeHandler, false);
+}
+
+function startBlocking() {
+    blocking = true;
+    log("Ads starting...");
+
+    miniVideo = document.querySelector("div[data-test-selector=picture-by-picture-player-container] video");
+    miniVideoContainer = document.querySelector("div[data-test-selector=picture-by-picture-player-background]");
+    muteButton = document.querySelector("button[data-a-target=player-mute-unmute-button]");
+    volumeSlider = document.querySelector("input[data-a-target=player-volume-slider]");
+
+    if (!miniVideo) {
+        adText.textContent = "No mini player found -- auto muting instead. ";
+        restoreVolume = mainVideo.volume;
+        mainVideo.muted = true;
+
+        if (muteButton && volumeSlider) {
+            muteButton.style.cssText = "display: none !important;";
+            volumeSlider.style.cssText = "display: none !important;";
+        }
+        return;
+    }
+
+    adText.textContent = "Blocking ads. ";
+    mainVideo.insertAdjacentElement("afterend", miniVideo);
+
+    miniVideo.muted = false;
+    miniVideo.volume = mainVideo.volume;
+    mainVideo.muted = true;
+
+    if (miniVideoContainer) {
+        miniVideoContainer.style.cssText = "display: none !important;";
+    }
+
+    if (muteButton && volumeSlider) {
+        muteButton.style.cssText = "display: none !important;";
+        volumeSlider.style.cssText = "display: none !important;";
+
+        customSlider = document.createElement("input");
+        customSlider.type = "range";
+        customSlider.min = 0;
+        customSlider.max = 1;
+        customSlider.step = 0.01;
+        customSlider.value = miniVideo.volume;
+        customSlider.className = "STA__custom-slider";
+
+        customSlider.addEventListener("input", function () {
+            miniVideo.volume = customSlider.value;
+        }, false);
+
+        muteButton.insertAdjacentElement("afterend", customSlider);
+    }
+}
+
+function stopBlocking() {
+    blocking = false;
+    log("Ads ending...");
+
+    if (restoreVolume) {
+        mainVideo.muted = restoreVolume === 0;
+        mainVideo.volume = restoreVolume;
+    }
+
+    if (miniVideo) {
+        mainVideo.muted = miniVideo.volume === 0;
+        mainVideo.volume = miniVideo.volume;
+        miniVideo.remove();
+    }
+
+    if (muteButton) {
+        muteButton.style.cssText = "";
+    }
+
+    if (volumeSlider) {
+        volumeSlider.style.cssText = "";
+    }
+
+    if (customSlider) {
+        customSlider.remove();
+    }
+
+    resetRefs();
+}
+
+log("Loaded");
