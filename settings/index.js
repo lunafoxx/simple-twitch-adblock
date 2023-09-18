9045351c778e7da060cc14f79d7315713e7c56a2
LydianLights <rane.fields@gmail.com>
2023-09-18 16:55:43 -0400

    Add button for permissions request

--- settings/index.js
+++ settings/index.js
@@ -1,7 +1,21 @@
 const enableCheckbox = document.querySelector("#enable-checkbox");
 const debugCheckbox = document.querySelector("#debug-checkbox");
+const permissionsMessage = document.querySelector("#no-permissions");
+const permissionsButton = document.querySelector("#permissions-button");
+
+permissionsButton.addEventListener("click", requestPermissions, true);
+
+async function requestPermissions() {
+    browser.permissions.request({ origins: ["*://www.twitch.tv/*"] });
+    window.close();
+}
 
 async function init() {
+    const hasPermissions = await browser.permissions.contains({ origins: ["*://www.twitch.tv/*"] });
+    if (!hasPermissions) {
+        permissionsMessage.className = "";
+    }
+
     // wtf is this api
     enableCheckbox.checked = (await browser.storage.sync.get("adblock-enabled"))["adblock-enabled"] ?? true;
     debugCheckbox.checked = (await browser.storage.sync.get("debug-enabled"))["debug-enabled"] ?? false;
